b65463653148e3269adab25b669149bc50a87eaf
iAugus <iAugux@gmail.com>
2016-01-27 21:57:46 +0800

    Initial Commit

---
+++ Phonetic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phonetic
+//
+//  Created by Augus on 1/27/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
