baa0bccbaf2e933c27fb78c5ed288cb8614397cd
iAugus <iAugux@gmail.com>
2016-03-23 16:51:23 +0800

    fixed: blur effect not working on early iPad

---
+++ Phonetic/Extensions/UIDevice+Extension.swift
@@ -0,0 +1,53 @@
+//
+//  UIDevice+Extension.swift
+//
+//  Created by Augus on 3/23/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - Check if device supports blur
+// REFERENCE: - http://stackoverflow.com/a/29997626/4656574
+
+extension UIDevice {
+    
+    func isBlurSupported() -> Bool {
+        var supported = Set<String>()
+        supported.insert("iPad")
+        supported.insert("iPad1,1")
+        supported.insert("iPhone1,1")
+        supported.insert("iPhone1,2")
+        supported.insert("iPhone2,1")
+        supported.insert("iPhone3,1")
+        supported.insert("iPhone3,2")
+        supported.insert("iPhone3,3")
+        supported.insert("iPod1,1")
+        supported.insert("iPod2,1")
+        supported.insert("iPod2,2")
+        supported.insert("iPod3,1")
+        supported.insert("iPod4,1")
+        supported.insert("iPad2,1")
+        supported.insert("iPad2,2")
+        supported.insert("iPad2,3")
+        supported.insert("iPad2,4")
+        supported.insert("iPad3,1")
+        supported.insert("iPad3,2")
+        supported.insert("iPad3,3")
+        
+        return !supported.contains(hardwareString())
+    }
+    
+    private func hardwareString() -> String {
+        var name: [Int32] = [CTL_HW, HW_MACHINE]
+        var size: Int = 2
+        sysctl(&name, 2, nil, &size, &name, 0)
+        var hw_machine = [CChar](count: Int(size), repeatedValue: 0)
+        sysctl(&name, 2, &hw_machine, &size, &name, 0)
+        
+        let hardware: String = String.fromCString(hw_machine)!
+        return hardware
+    }
+    
+}
---
+++ Phonetic/UIEffectView+Ex.swift
@@ -0,0 +1,44 @@
+//
+//  UIEffectView+Ex.swift
+//  Phonetic
+//
+//  Created by Augus on 3/23/16.
+//  Copyright © 2016 iAugus. All rights reserved.
+//
+
+import UIKit
+
+
+/// fixes: blur effect not working on early iPad.
+
+private var xoAssociationKey: UInt8 = 0
+
+extension UIVisualEffectView {
+    
+    private var subview: UIView! {
+        get {
+            return objc_getAssociatedObject(self, &xoAssociationKey) as? UIView
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &xoAssociationKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+    
+    public override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        
+        // Testing on Real Device, it won't affect on Simulator
+        guard !UIDevice.currentDevice().isBlurSupported() else { return }
+        
+        effect = nil
+        subview = UIView(frame: frame)
+        subview.backgroundColor = UIColor(red: 0.498, green: 0.498, blue: 0.498, alpha: 0.926)
+        addSubview(subview)
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        subview?.frame = bounds
+    }
+    
+}
